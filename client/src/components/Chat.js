4f3f5116ee02c8f6e2d7af91a5de943563d2add9
Bogdan Eremin <55001168+kixygod@users.noreply.github.com>
2023-10-28 15:51:57 +0700

    Гумеров websocket

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -6,14 +6,16 @@ import Message from './Message'; // Импортируйте компонент 
 const Chat = () => {
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
+    const [userName, setUserName] = useState('');
+
 
     const socket = useMemo(() => {
         return io('ws://localhost:8080');
     }, []);
 
     useEffect(() => {
-        socket.onmessage = (event) => {
-            const message = event.data;
+        socket.on = (event) => {
+            const message = JSON.parse(event.data)
             setMessages((prevMessages) => [...prevMessages, { text: message, isServer: true }]);
         };
 
@@ -27,9 +29,18 @@ const Chat = () => {
             return;
         }
 
-        socket.send(newMessage);
-        setMessages((prevMessages) => [...prevMessages, { text: newMessage, isServer: false }]);
+        const messageObject = { event: 'message', message: newMessage, user: userName };
+        socket.send(JSON.stringify(messageObject));
+        setMessages((prevMessages) => [...prevMessages, { text: newMessage, isServer: false, user: userName }]);
         setNewMessage('');
+    };
+
+    const handleSetUserName = () => {
+        if (userName.trim() !== '') {
+            // Отправить имя на сервер (если требуется)
+            // Начать чат
+            setUserName(userName);
+        }
     };
 
     const handleKeyPress = (e) => {
@@ -40,21 +51,37 @@ const Chat = () => {
 
     return (
         <div style={{ fontFamily: 'Roboto, sans-serif', fontSize: '1.5rem' }}>
-            <Link to="/">Перейти к задачам</Link>
-            <div className="message-container">
-                {messages.map((message, index) => (
-                    <Message key={index} text={message.text} isServer={message.isServer} />
-                ))}
-            </div>
-            <input
-                type="text"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                onKeyPress={handleKeyPress}
-            />
-            <button onClick={handleSendMessage}>Отправить</button>
+            {userName ? (
+                <>
+                    <Link to="/">Перейти к задачам</Link>
+                    <div className="message-container">
+                        {/* Отображение сообщений чата */}
+                    </div>
+                    <input
+                        type="text"
+                        value={newMessage}
+                        onChange={(e) => setNewMessage(e.target.value)}
+                        onKeyPress={handleKeyPress}
+                    />
+                    <button onClick={handleSendMessage}>Отправить</button>
+                </>
+            ) : (
+                <div>
+                    <p>Пожалуйста, введите ваше имя:</p>
+                    <input
+                        type="text"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <button onClick={handleSetUserName}>Войти в чат</button>
+                </div>
+            )}
         </div>
     );
+
+
+
+
 };
 
 export default Chat;
--- server/src/api/websocket.js
+++ server/src/api/websocket.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const testapp = express();
 const server = require('http').createServer(testapp);
-const WebSocket = require('ws');
+const WebSocketServer = require('ws');
 var cors = require('cors');
 testapp.use(cors());
 
 const wss = require('socket.io')(server, { cors: { origin: "*" } });
+
 
 console.log('WebSocket-сервер работает на порту 8080');
 console.log('Ожидание подключений клиентов...');
@@ -15,13 +16,28 @@ wss.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.on('message', (message) => {
+        message = JSON.parse(message)
         console.log(`Received: ${message}`);
-        ws.send(`You sent: ${message}`);
+
+        switch (message.event) {
+            case 'message':
+                broadCastMessage(message)
+                break
+            case 'connection':
+                broadCastMessage(message)
+                break
+        }
     });
 
     // Отправка сообщения клиенту
     ws.send('Привет, клиент!');
 });
+
+function broadCastMessage(message) {
+    wss.clients.forEach(user => {
+        user.send(JSON.stringify(message))
+    })
+}
 
 
 testapp.get('/', (req, res) => res.send('Hello world'));
