9f673e1abbd61ddd00ba23e370943a2051a9adb6
Bogdan Eremin <55001168+kixygod@users.noreply.github.com>
2023-11-25 00:34:41 +0700

    fix: deleted unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./style.css"
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import axios from 'axios';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
